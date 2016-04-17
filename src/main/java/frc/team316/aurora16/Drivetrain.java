6e77a23443184c2488fe457e400d6ef10295c223
Christian Steward <csteward24@gmail.com>
2016-04-16 23:47:09 -0400

    Added gyro

--- src/main/java/frc/team316/aurora16/Drivetrain.java
+++ src/main/java/frc/team316/aurora16/Drivetrain.java
@@ -4,7 +4,7 @@ package frc.team316.aurora16;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
-
+//import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 /**
  * Drivetrain contains all the code related to driving the robot. <br>
  * Right now it only abstracts a few methods to make teleop more readable.
@@ -19,6 +19,7 @@ public class Drivetrain {
     Talon frontRight = new Talon(0);
     RobotDrive driveMotors = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
     Joystick driveStick = new Joystick(1);
+   // ADXRS450_Gyro gyro = new ADXRS450_Gyro();
     
 	/**
 	 * Default constructor
