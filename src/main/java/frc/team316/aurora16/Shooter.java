1db3f4a6328bf0fa4b1dc051f7caacf45c00c99f
Christian Steward <csteward24@gmail.com>
2016-04-17 01:02:14 -0400

    end of day 4-17

--- src/main/java/frc/team316/aurora16/Shooter.java
+++ src/main/java/frc/team316/aurora16/Shooter.java
@@ -3,11 +3,22 @@
  */
 package frc.team316.aurora16;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Counter;
+
 /**
  * @author Christian
  *
  */
 public class Shooter {
+	
+	CANTalon flywheelTalon = new CANTalon(2);
+	CANTalon angleTalon = new CANTalon(1);
+	Talon shooterCam = new Talon(5);
+	Counter rpmCounter = new Counter(0);
 	
 	//constructor
 	public void shooter() {
